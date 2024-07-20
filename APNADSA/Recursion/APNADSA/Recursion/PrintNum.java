9d1179b4ba6f082c336f2fea2b557cca194d0334
VVshivasai <21a81a0253@sves.org.in>
2024-07-20 10:32:05 +0530

    first commit

---
+++ APNADSA/Recursion/APNADSA/Recursion/PrintNum.java
@@ -0,0 +1,20 @@
+package APNADSA.Recursion;
+
+// print n to 1 numbers
+
+
+public class PrintNum {
+    static void Print(int n){
+        if(n==1){ // Base Case
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        Print(n-1); // Call previous number to function
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        Print(n);
+    }
+
+}
